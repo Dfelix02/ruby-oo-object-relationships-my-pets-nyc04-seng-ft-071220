7c0561fd38a1230d64ec61c5133632d452b0d47d
wilsonng21 <wilsongch92@gmail.com>
2020-08-05 16:39:25 -0400

    all tests passing

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
   # code goes here
+  attr_accessor :owner, :mood 
+  attr_reader :name
+  
+  @@all = []
+
+  def initialize(name_param, owner_param)
+    @name = name_param
+    @owner = owner_param
+    @mood = "nervous"
+    @@all << self
+  end
+
+
+  def self.all 
+    @@all 
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,78 @@
+require 'pry'
+
+require_relative "./dog"
+require_relative "./cat"
+
 class Owner
   # code goes here
+  attr_reader :name, :species
+
+  @@all = []
+
+  def initialize(name_param)
+    @name = name_param
+    @species = "human"
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{self.species}."
+  end
+
+  def self.all
+    @@all 
+  end
+
+  def self.count 
+    @@all.length
+  end
+
+  def self.reset_all 
+    @@all.clear
+  end
+
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+    # binding.pry
+  end
+
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+    # binding.pry
+  end
+
+  def buy_cat(cat_name)
+    Cat.new(cat_name, self)
+    # binding.pry
+  end
+
+  def buy_dog(dog_name)
+    Dog.new(dog_name, self)
+    # binding.pry
+  end
+
+  def walk_dogs
+    self.dogs.select {|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats 
+    self.cats.select { |cat| cat.mood = "happy"}
+  end
+
+  def sell_pets 
+    self.dogs.select {|dog| dog.mood = "nervous"}
+    self.cats.select { |cat| cat.mood = "nervous"}
+    self.dogs.select {|dog| dog.owner = nil}
+    self.cats.select { |cat| cat.owner = nil}
+  end
+
+  def list_pets 
+    "I have #{self.dogs.size} dog(s), and #{self.cats.size} cat(s)."
+  end
+
 end
+
+
+
+# binding.pry
+0
